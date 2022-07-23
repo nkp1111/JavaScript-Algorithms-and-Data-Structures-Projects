caa2fd475866d4b40cb68cfb07025faf0e3df0ff
nkp1111 <105532413+nkp1111@users.noreply.github.com>
2022-07-23 19:25:38 +0530

    Create Roman Numeral Converter.js

---
+++ Roman Numeral Converter.js
@@ -0,0 +1,33 @@
+/*
+Convert the given number into a roman numeral.
+
+Roman numerals	Arabic numerals
+M	1000
+CM	900
+D	500
+CD	400
+C	100
+XC	90
+L	50
+XL	40
+X	10
+IX	9
+V	5
+IV	4
+I	1
+All roman numerals answers should be provided in upper-case.
+*/
+
+function convertToRoman(num) {
+  let convertValue = {"M":1000,"CM":900,"D":500,"CD":400,"C":100,"XC":90,"L":50,"XL":40,"X":10,"IX":9,"V":5,"IV":4,"I":1};
+  let romanNumeral = "";
+  for (let i in convertValue) {
+    while (convertValue[i] <= num) {
+      romanNumeral += i;
+      num -= convertValue[i];
+    }
+  }
+ return romanNumeral;
+}
+
+convertToRoman(36);
